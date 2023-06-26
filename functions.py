cfe574ff02c7dc3b667bee2fadebe5d366b3deac
Laura Edwards <lledwards@smith.edu>
2023-06-25 22:40:34 -0400

    Added progress function, cleaned up some function print checks, fixed print formatting

--- functions.py
+++ functions.py
@@ -17,7 +17,6 @@ def get_school_dist_page(district_id: str):
     else:
         # for districts with 1 page, there is no text to display Page Number of Number, so page number is 1
         page_count = "1"
-    print(page_count)
     if int(page_count) > 1:
         for page_num in range(2, int(page_count) + 1):
             add_page = requests.get(f"https://nces.ed.gov/ccd/schoolsearch/school_list.asp?Search=1&DistrictID={district_id}&SchoolPageNum={page_num}").text
@@ -34,8 +33,8 @@ def get_school_eligibility(district_id: str, elig_list: list):
     """
     with open("district_info.txt", "r") as district_info:
         contents = district_info.read()
+    # find links to all schools in district
     results = re.findall(rf"school_detail\.asp\?Search=1&DistrictID={district_id}.*?&ID={district_id}\d*", contents)
-    print(len(results))
     for query_param in results:
         school_url = f"https://nces.ed.gov/ccd/schoolsearch/{query_param}"
         school_page = requests.get(school_url).text
@@ -45,27 +44,23 @@ def get_school_eligibility(district_id: str, elig_list: list):
         if bool(total_large):
             eligible_raw = re.search('[0-9]+,[0-9]+', total_large[0]).group(0)
             eligible = eligible_raw.replace(",", "")
-            print(eligible)
             elig_list.append(int(eligible))
             continue
         if bool(total_indep_large):
             eligible_raw = re.search('[0-9]+,[0-9]+', total_indep_large[0]).group(0)
             eligible = eligible_raw.replace(",", "")
-            print(eligible)
             elig_list.append(int(eligible))
             continue
         # For schools with eligible students between 1-999
         total = re.findall(r"<strong>Total<sup>1</sup>: </strong>\d+", school_page)
         if bool(total):
             eligible = re.match('.*?([0-9]+)$', total[0]).group(1)
-            print(eligible)
             elig_list.append(int(eligible))
         else:
             total_indep = re.findall("<strong>Free lunch eligible by Direct Certification<sup>2</sup>: </strong>\d+", school_page)
             eligible = re.match('.*?([0-9]+)$', total_indep[0]).group(1)
-            print(eligible)
             elig_list.append(int(eligible))
-        return
+    return
 
 def calculate_percent_elig(district_id: str, total_list: list, elig_list: list):
     """
@@ -76,6 +71,7 @@ def calculate_percent_elig(district_id: str, total_list: list, elig_list: list):
     """
     with open("district_info.txt", "r") as district_info:
         contents = district_info.read()
+    # find links to all schools in district
     results = re.findall(rf"school_detail\.asp\?Search=1&DistrictID={district_id}.*?&ID={district_id}\d*", contents)
     total_number_of_schools = len(results)
     for query_param in results:
@@ -85,38 +81,57 @@ def calculate_percent_elig(district_id: str, total_list: list, elig_list: list):
         total_students_find = re.findall(
             "<td align=\"left\"><strong><font size=\"2\">Total Students:</font></strong></td>\r\n\t\t<td><img border=\"0\" src=\"/ccd/commonfiles/images/spacer\.gif\" width=\"4\" height=\"10\"></td>\r\n\t\t<td align=\"right\"><font size=\"3\">[0-9]+",
             school_page)
+        # for schools with total students 1000-999999
         if bool(total_students_find_large):
             total_students_raw = re.search('[0-9]+,[0-9]+', total_students_find_large[0]).group(0)
             total_students = total_students_raw.replace(",", "")
-            print(total_students)
             total_list.append(int(total_students))
+        # for schools with students from 1-999
         elif bool(total_students_find):
             total_students = re.search('([0-9]+)$', total_students_find[0]).group(1)
-            print(total_students)
             total_list.append(int(total_students))
         else:
-            print("cant find anything")
+            print("Cannot find total students enrolled...exiting program")
+            return
     count = 0
+    # check lengths of both lists to ensure that no school data is missing
     if len(elig_list) == len(total_list):
         for index,elig_number in enumerate(elig_list):
             percent = elig_number / total_list[index]
-            print(percent)
             if percent >= 0.7:
                 count += 1
-        percent_qual = count / total_number_of_schools
-        print(percent_qual)
+        percent_qual = (count / total_number_of_schools) * 100
+        print(f"{percent_qual:.2f}% of schools in this district meet or exceed 70% or more students participating in the National School Lunch Program.")
     else:
         print("Some school data is missing. Operation cannot be completed at this time.")
     return
 
+def clear_district_info(district_info):
+    """
+    Clears district info text from last use of program
+    :param district_info: file: File containing district info
+    :return: Nothing
+    """
+    with open(district_info, "w") as district_file:
+        district_file.write("")
+
+def progress():
+    """
+    Print 'working' text for comfortable user experience
+    :return:
+    """
+    print("Working...")
+    return
 def main():
+    clear_district_info("district_info.txt")
     elig = []
     total = []
-    district_id = input("What is the NCES District ID?")
+    district_id = input("What is the NCES District ID? ")
     dist_info = get_school_dist_page(district_id)
+    progress()
     get_school_eligibility(district_id, elig)
+    progress()
     calculate_percent_elig(district_id, total, elig)
-main()
 
 
 
