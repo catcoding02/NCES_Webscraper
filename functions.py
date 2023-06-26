c2c8469182bba074f9f8362f9d56bcb2fb5bfc1b
Laura Edwards <lledwards@smith.edu>
2023-06-26 11:22:59 -0400

    Added state dictionary

--- functions.py
+++ functions.py
@@ -1,6 +1,26 @@
 import requests
 import re
 
+def get_county_school_dist_page(county_name: str, state_name:str):
+    state_name = {"01": 'Alabama', "02": 'Alaska', "04": 'Arizona',
+"05": 'Arkansas', "06": 'California', "08": 'Colorado',
+"09": 'Connecticut', "10": 'Delaware', "11": 'District of Columbia',
+"12": 'Florida', "13": 'Georgia', "15": 'Hawaii', "16": 'Idaho',
+"17": 'Illinois', "18": 'Indiana', "19": 'Iowa',
+"20": 'Kansas', "21": 'Kentucky', "22": 'Louisiana',
+"23": 'Maine', "24": 'Maryland', "25": 'Massachusetts',
+"26": 'Michigan', "27": 'Minnesota', "28": 'Mississippi',
+"29": 'Missouri', "30": 'Montana', "31": 'Nebraska',
+"32": 'Nevada', "33": 'New Hampshire', "34": 'New Jersey',
+"35": 'New Mexico', "36": 'New York', "37": 'North Carolina',
+"38": 'North Dakota', "39": 'Ohio', "40": 'Oklahoma',
+"41": 'Oregon', "42": 'Pennsylvania', "44": 'Rhode Island',
+"45": 'South Carolina', "46": "South Dakota", "47": 'Tennessee',
+"48": 'Texas', "49": 'Utah', "50": 'Vermont',
+"51": 'Virginia', "53": 'Washington', "54": "West Virginia",
+"55": 'Wisconsin', "56": 'Wyoming'
+}
+    county_school_dist_url = f"https://nces.ed.gov/ccd/districtsearch/district_list.asp?Search=1&details=1&State=25&County={county_name}&DistrictType=1&DistrictType=2&DistrictType=3&DistrictType=4&DistrictType=5&DistrictType=6&DistrictType=7&DistrictType=8&DistrictType=9&NumOfStudentsRange=more&NumOfSchoolsRange=more&DistrictPageNum=1"
 def get_school_dist_page(district_id: str):
     """
     Gets school district school list page
@@ -58,11 +78,16 @@ def get_school_eligibility(district_id: str, elig_list: list):
             elig_list.append(int(eligible))
         else:
             total_indep = re.findall("<strong>Free lunch eligible by Direct Certification<sup>2</sup>: </strong>\d+", school_page)
-            eligible = re.match('.*?([0-9]+)$', total_indep[0]).group(1)
-            elig_list.append(int(eligible))
+            if total_indep:
+                eligible = re.match('.*?([0-9]+)$', total_indep[0]).group(1)
+                if eligible == "0":
+                    print("This school has not provided data. Skipping school...")
+                    continue
+                else:
+                    elig_list.append(int(eligible))
     return
 
-def calculate_percent_elig(district_id: str, total_list: list, elig_list: list, county_agg: str, total_number_of_eligible_students_in_county: list, total_number_of_schools_in_county: list):
+def calculate_percent_elig(district_id: str, total_list: list, elig_list: list, county_agg: str):
     """
     :param district_id: str: NCES District ID
     :param total_list: list: list to be appended with total students in each school in district
@@ -92,7 +117,11 @@ def calculate_percent_elig(district_id: str, total_list: list, elig_list: list, 
         # for schools with students from 1-999
         elif bool(total_students_find):
             total_students = re.search('([0-9]+)$', total_students_find[0]).group(1)
-            total_list.append(int(total_students))
+            if total_students == "0":
+                print("This school has has not reported data. Skipping...")
+                continue
+            else:
+                total_list.append(int(total_students))
         else:
             print("Cannot find total students enrolled...exiting program")
             return
@@ -104,8 +133,8 @@ def calculate_percent_elig(district_id: str, total_list: list, elig_list: list, 
             if percent >= 0.7:
                 count += 1
         if county_agg == "1":
-            write_to_total_files("total_elig")
-            total_number_of_schools_in_county.append(total_number_of_schools)
+            write_to_total_files("total_elig_schools.txt", str(count))
+            write_to_total_files("total_schools.txt", str(total_number_of_schools))
         percent_qual = (count / total_number_of_schools) * 100
         print(f"{percent_qual:.2f}% of schools in this district meet or exceed 70% or more students participating in the National School Lunch Program.")
     else:
@@ -114,15 +143,15 @@ def calculate_percent_elig(district_id: str, total_list: list, elig_list: list, 
 
 def write_to_total_files(total_filename, value):
     with open(total_filename, "a") as total:
-        total.write(value)
-def clear_district_info(district_info):
+        total.write(value + "\n")
+def clear_file(filename):
     """
     Clears district info text from last use of program
     :param district_info: file: File containing district info
     :return: Nothing
     """
-    with open(district_info, "w") as district_file:
-        district_file.write("")
+    with open(filename, "w") as clear_file:
+        clear_file.write("")
 
 def progress():
     """
@@ -135,15 +164,17 @@ def progress():
 def main():
     elig = []
     total = []
-    total_number_of_eligible_students_in_county = []
-    total_number_of_schools_in_county = []
-    county_agg = input("Will you be reporting by county (option 1) or by district (option 2)? Choose option 1 or 2: ")
+    new_user = input("Is this a new entry? Y or N: ")
+    if new_user == "Y":
+        clear_file("total_schools.txt")
+        clear_file("total_elig_schools.txt")
+    county_agg = input("Are you planning to report for a whole county (option 1) or a single district (option 2)? Choose option 1 or 2: ")
     district_id = input("What is the NCES District ID? ")
     dist_info = get_school_dist_page(district_id)
     progress()
     get_school_eligibility(district_id, elig)
     progress()
-    calculate_percent_elig(district_id, total, elig, total_number_of_eligible_students_in_county, total_number_of_schools_in_county)
+    calculate_percent_elig(district_id, total, elig, county_agg)
 
 
 
